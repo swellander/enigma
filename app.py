d96e9db6e3add7ee9885936e12d025fb39cb88c0
Sam Wellander <samwellander@gmail.com>
2020-05-17 18:23:34 -0700

    Add method to create rotors

--- app.py
+++ app.py
@@ -2,21 +2,40 @@ from random import randrange
 
 
 class Enigma(object):
+    num_potential_chars = 128
 
     def __generate_random_ascii_char_list(self) -> list:
         """
         Returns a list of ASCII character strings in a randomized order.
-        Used for creating scramblers.
+        Used for creating rotors.
         """
 
         ordered_list_of_chars = [chr(i)
-                                 for i in range(127)]  # ASCII codes 0-127
+                                 for i in range(self.num_potential_chars)]
         rand_list_of_chars = []
 
-        for _ in range(127):
+        for _ in range(self.num_potential_chars):
             j = randrange(len(ordered_list_of_chars))
             char = ordered_list_of_chars[j]
             rand_list_of_chars.append(char)
             ordered_list_of_chars.remove(char)
 
         return rand_list_of_chars
+
+    def __generate_rotor(self) -> list:
+        """
+        Returns a list of tuples representing an enigma rotor,
+        where the first value of the tuple is the input and the
+        second value is the output. https://en.wikipedia.org/wiki/Enigma_rotor_details
+        """
+        front = self.__generate_random_ascii_char_list()
+        back = self.__generate_random_ascii_char_list()
+        rotor = []
+
+        for i in range(self.num_potential_chars):
+            rotor.append((
+                front[i],
+                back[i],
+            ))
+
+        return rotor
