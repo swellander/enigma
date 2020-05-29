c67c65bb946bd27b113127e2ffdcafd0b1942e55
Sam Wellander <samwellander@gmail.com>
2020-05-28 20:27:57 -0700

    Rename app.py

---
+++ enigma.py
@@ -0,0 +1,167 @@
+from random import randrange
+from math import floor
+
+
+class Enigma(object):
+
+    num_potential_chars = 95
+    num_rotors = 3
+    step = 0
+
+    def __init__(self):
+        rotor_1 = self.__generate_rotor()
+        rotor_2 = self.__generate_rotor()
+        rotor_3 = self.__generate_rotor()
+        self.reflector = self.__generate_rotor()
+        self.rotors = (rotor_1, rotor_2, rotor_3)
+
+    def encrypt_text(self, text: str) -> str:
+        encrypted_text = ''
+        for char in text:
+            encrypted_text += self.__encrypt_char(char)
+            self.step += 1
+
+        self.__reset_step_counter()
+        return encrypted_text
+
+    def decrypt_text(self, text: str) -> str:
+        decrypted_text = ''
+        for char in text:
+            decrypted_text += self.__decrypt_char(char)
+            self.step += 1
+
+        self.__reset_step_counter()
+        return decrypted_text
+
+    def __reset_step_counter(self):
+        """
+        Resets the rotor counter to 0. To be used after encrypting
+        an entire message.
+        """
+
+        self.step = 0
+
+    def __encrypt_char(self, char: str) -> str:
+        """
+        Pass character forward through rotors.
+        """
+
+        return self.__process_char(char)
+
+    def __decrypt_char(self, char: str) -> str:
+        """
+        Pass character backward through rotors.
+        """
+
+        return self.__process_char(char, reflected=True)
+
+    def __process_char(self, char: str, reflected=False) -> str:
+        """
+        Passes a character through rotors, changing its value
+        for each rotor.
+        """
+        rotor_listing = [(idx, rotor) for idx, rotor in enumerate(self.rotors)]
+        for rotor_num, rotor in rotor_listing:
+            char = self.__convert_char(
+                char=char,
+                rotor=rotor,
+                rotor_num=rotor_num,
+                reverse=reflected)
+
+        char = self.__convert_char(
+            char=char,
+            rotor=self.reflector,
+            reflected=reflected,
+            rotor_num=len(rotor_listing),
+            reverse=reflected)
+
+        for rotor_num, rotor in rotor_listing[::-1]:
+            char = self.__convert_char(
+                char=char,
+                rotor=rotor,
+                reflected=True,
+                rotor_num=rotor_num,
+                reverse=reflected)
+
+        return char
+
+    def __convert_char(self, *, char: str, rotor: list, rotor_num: int, reverse: bool, reflected=False) -> str:
+        """ Passes char through an individual rotor"""
+
+        front = 1 if reflected else 0
+        back = 0 if reflected else 1
+        rotor_length = len(rotor)
+        shift = floor(self.step / rotor_length**rotor_num)
+
+        for idx, input_mapping in enumerate(rotor):
+            if input_mapping[front] == char:
+                if reverse:
+                    total_shift = idx - shift
+                else:
+                    total_shift = idx + shift
+
+                output_mapping_idx = total_shift % rotor_length
+                output_char = rotor[output_mapping_idx][back]
+                return output_char
+
+    def __generate_rotor(self) -> list:
+        """
+        Returns a list of tuples representing an enigma rotor,
+        where the first value of the tuple is the input (front) and the
+        second value is the output (back). https://en.wikipedia.org/wiki/Enigma_rotor_details
+        """
+
+        front = self.__generate_random_ascii_char_list()
+        back = self.__generate_random_ascii_char_list()
+        rotor = []
+
+        for i in range(len(front)):
+            rotor.append((
+                front[i],
+                back[i],
+            ))
+
+        return rotor
+
+    def __generate_random_ascii_char_list(self) -> list:
+        """
+        Returns a list of ASCII character strings in a randomized order.
+        Used for creating rotors.
+        """
+
+        ordered_list_of_chars = [chr(i)
+                                 for i in range(32, 128) if chr(i)]
+        num_potential_chars = len(ordered_list_of_chars)
+        rand_list_of_chars = []
+
+        for _ in range(num_potential_chars):
+            try:
+                j = randrange(len(ordered_list_of_chars))
+            except ValueError:
+                import pdb
+                pdb.set_trace()
+            char = ordered_list_of_chars[j]
+            rand_list_of_chars.append(char)
+            ordered_list_of_chars.remove(char)
+
+        return rand_list_of_chars
+
+
+def main():
+    enigma = Enigma()
+
+    msg_to_encrypt = input("   Enter message to be encrypted: ")
+    encrypted_msg = enigma.encrypt_text(msg_to_encrypt)
+
+    print("Encrypted message: ", encrypted_msg)
+    decrypted_msg = enigma.decrypt_text(encrypted_msg)
+
+    print("Decrypted message: ", decrypted_msg)
+
+
+if __name__ == "__main__":
+    main()
+
+# a f  a x
+# b w  b s
+# c e  c h
